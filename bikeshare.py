61fd6131f1092eef991157de70c89c94e8792aa1
Fred Motari <faruyam@gmail.com>
2019-02-06 13:14:18 -0600

    Updated show_raw_data function to dispaly ten rows at a time

--- bikeshare.py
+++ bikeshare.py
@@ -29,9 +29,9 @@ def get_filters():
     allowed_months = ('january','february','march','april','may','june')
 
     allowed_days = ('mon','tue','wed','thur','fri','sat','sun')
-    
+
     weekday_dict = {"Mon": 'Monday',"Tue": 'Tuesday',"Wed": 'Wednesday',"Thur": 'Thursday',"Fri": 'Friday',"Sat": 'Saturday',"Sun": 'Sunday'}
-    
+
     print('\nHello! Let\'s explore some US bikeshare data! \n')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     print("Would you like to see data for Chicago, New York, or Washington cities? \n")
@@ -41,15 +41,15 @@ def get_filters():
         city = input("Enter city : \n")
     else:
         user_filters.append(city.title())
-    
+
     print("Would you like to filter {}'s  data by month, day, both or not at all? Type none for no time filter.\n".format(str(user_filters[0])))
-    
+
     time_filter_option = input('Enter your time filtering option: \n').rstrip().lstrip()
-    
+
     while time_filter_option not in allowed_time_filters:
         print('You need to enter valid time filtering choice eg by month, day, both or not at all? Type none for no time filter.')
         time_filter_option = input('Enter your time filtering option: ').rstrip().lstrip()
-    
+
     if time_filter_option.lower() == 'both':
         # get user input for month (all, january, february, ... , june)
         month = input("Enter month : ").rstrip().lstrip()
@@ -58,7 +58,7 @@ def get_filters():
             month = input("Enter month : ").rstrip().lstrip()
         else:
             user_filters.append(month.title())
-            
+
         # get user input for day of week (all, monday, tuesday, ... sunday)
         day = input("Enter weekday eg  Mon, Tue, Wed, Thur, Fri, Sat, Sun: ").title().rstrip().lstrip()
         while day.lower() not in allowed_days:
@@ -66,7 +66,7 @@ def get_filters():
             day = input("Enter weekday abbreviation eg  Mon, Tue, Wed, Thur, Fri, Sat, Sun: ").title().rstrip().lstrip()
         else:
             user_filters.append(weekday_dict[day].title())
-    
+
     elif time_filter_option.lower() == 'month':
         month = input("Enter month name, January through June: ").rstrip().lstrip()
         while month.lower() not in allowed_months:
@@ -74,8 +74,8 @@ def get_filters():
             month = input("Enter month : ").rstrip().lstrip()
         else:
             user_filters.append(month.title())
-            user_filters.append('all')        
-    
+            user_filters.append('all')
+
     elif time_filter_option.lower() == 'day':
         day = input("Enter weekday eg  Mon, Tue, Wed, Thur, Fri, Sat, Sun: ").title().rstrip().lstrip()
         while day.lower() not in allowed_days:
@@ -183,24 +183,24 @@ def station_stats(df):
     df0 = df.groupby(['Start Station']).size().reset_index().rename(columns={0:'start station count'})
     max_start_station_count = str(df0['start station count'].max())
     print('\tMost commonly used start station: {} and it was used {} times \n'.format(popular_station,max_start_station_count))
-    
+
     # display most commonly used end station
     # count how many times a particular end station occurs in data frame.
     popular_end_station = df['End Station'].mode()[0]
     df01 = df.groupby(['End Station']).size().reset_index().rename(columns={0:'end station count'})
     max_end_station_count = str(df01['end station count'].max())
     print('\tMost commonly used end station: {} and it was used {} times \n'.format(popular_end_station,max_end_station_count))
-    
+
     # display most frequent combination of start station and end station trip
     # group by start and end station pair and count how many times the pair occurred.
     df1 = df.groupby(['Start Station','End Station']).size().reset_index().rename(columns={0:'count'})
-    
+
     # convert df1 series object type to dictionary for easy access of dict elements
     df2 = df1.loc[df1['count'].idxmax()].to_dict()
-    
+
     print("\tThe combination of Start station: '{}' and end station: '{}' were the most frequently used. \n\tThis combination of stations  were used {} times. ".format(df2['Start Station'],df2['End Station'],str(df2['count'])))
-    
-    # Print the time taken to process statistics.      
+
+    # Print the time taken to process statistics.
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
@@ -213,7 +213,7 @@ def trip_duration_stats(df):
     # display total travel time
     total_trip_time = str(df['Travel Time'].sum())
     print('\tThe total time for all trips made is : ' + total_trip_time)
-    
+
     # display mean travel time
     trip_time_mean = str(df['Travel Time'].mean())
     print('\n\tThe average travel time is : ' + trip_time_mean)
@@ -230,13 +230,13 @@ def user_stats(df):
 
     # Display counts of user types
     print('The following are the counts of the types of users that hired bikes: \n')
-        
+
     df3 = df.dropna(axis=0,subset=['User Type'])
-    
+
     user_type_counts = df3['User Type'].value_counts().to_dict()
-    
+
     unique_user_types = df3['User Type'].unique().tolist()
-           
+
     for unique_user_type in unique_user_types:
         print('\tThere is/are ' + str(user_type_counts[unique_user_type]) + ' count/s of user type: ' + unique_user_type + '\n')
 
@@ -245,61 +245,61 @@ def user_stats(df):
         df4 = df.dropna(axis=0,subset=['Gender'])
         gender_counts = df4['Gender'].value_counts().to_dict()
         genders = df4['Gender'].unique().tolist()
-        
+
         for gender in genders:
             print('\tThere are ' + str(gender_counts[gender]) + ' counts of gender type: ' + gender + '\n')
-        
+
         # Display earliest, most recent, and most common year of birth
         print('These are earliest, most recent, and most common year of birth: \n')
-        
+
         print('\tThe earliest year of birth is : ' + str(int(df4['Birth Year'].min())) + '\n')
-        
+
         print('\tThe most recent year of birth is : ' + str(int(df4['Birth Year'].max())) + '\n')
-        
+
         print('\tThe most common year of birth is : ' + str(int(df4['Birth Year'].mode())) + '\n')
-                       
+
     else:
         print('\tSorry Washington does not have gender and birth based data.')
-    
-    # Print the time taken to process statistics. 
+
+    # Print the time taken to process statistics.
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
-    
+
 def show_raw_data(df):
     """
     Displays raw data for the filter option given. Slices of five rows of raw data are shown when a user types 'y' for yes
     and quits displaying raw data when 'n' for no is typed in prompt.
     """
-    
+
     # Drop columns that I added to help me answer descriptive statistics questions.
     df1 = df.drop(columns=['Travel Time','Start Hour','End Hour','month','day_of_week'])
     # Get the length of data frame to avoid out of range index errors.
     df_row_count = df1.shape[0]
     lower_index = 0
-    upper_index = 5
+    upper_index = 10
     allowed_user_choices = ['y','n']
     # Ensure that user response is either y or n.
-    list_next_five_rows = input("\nDo you want to display sets of five rows of raw data? Enter 'y' for yes or 'n' for no: ").lower().rstrip().lstrip()
+    list_next_five_rows = input("\nDo you want to display sets of ten rows of raw data? Enter 'y' for yes or 'n' for no: ").lower().rstrip().lstrip()
     while list_next_five_rows not in allowed_user_choices:
         print("You need to type in 'y' for yes or 'n' for no.")
-        list_next_five_rows = input("\nDo you want to display sets of five rows of raw data? Enter 'y' for yes or 'n' for no: ").lower().rstrip().lstrip()
-    
-    #Loop for as long as rows are within the index range of data frame.    
+        list_next_five_rows = input("\nDo you want to display sets of ten rows of raw data? Enter 'y' for yes or 'n' for no: ").lower().rstrip().lstrip()
+
+    #Loop for as long as rows are within the index range of data frame.
     while upper_index < df_row_count:
         if list_next_five_rows == 'y':
             print(df1[lower_index:upper_index])
             lower_index = upper_index
-            upper_index += 5
-            list_next_five_rows = input("\nDo you want to display sets of five rows of raw data? Enter 'y' for yes or 'n' for no: \n").lower().rstrip().lstrip()
+            upper_index += 10
+            list_next_five_rows = input("\nDo you want to display sets of ten rows of raw data? Enter 'y' for yes or 'n' for no: \n").lower().rstrip().lstrip()
             while list_next_five_rows not in allowed_user_choices:
                 print("You need to type in 'y' for yes or 'n' for no.")
-                list_next_five_rows = input('\nDo you want to display sets of five rows of raw data? ').lower().rstrip().lstrip()
+                list_next_five_rows = input('\nDo you want to display sets of ten rows of raw data? ').lower().rstrip().lstrip()
         else:
             print('\nYou have either opted not to proceed showing raw data or you have reached the end of raw data.')
             lower_index = 0
-            upper_index = 5
-            break    
-        
+            upper_index = 10
+            break
+
 def main():
     while True:
         city, month, day = get_filters()
